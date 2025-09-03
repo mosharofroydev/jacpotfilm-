42e6706af7ce5ce50cb9fa65c857f4e57da0b06b
mosharofroydev <mosharofroydev@gmail.com>
2025-09-03 15:23:14 +0000

    Create ads.py

---
+++ ads.py
@@ -0,0 +1,7 @@
+# ads.py
+
+ad_links = [
+    "🔥 আমাদের চ্যানেলে জয়েন করুন: https://t.me/your_channel",
+    "💥 Sponsor Link: https://example.com/ad1",
+    "👉 এখানে ক্লিক করুন: https://example.com/ad2"
+]
