25b9466e14268a03154ab999dfd445d4a7cb8976
mosharofroydev <mosharofroydev@gmail.com>
2025-09-05 08:41:18 +0000

    main.py

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
+ import pymongo
 
+ client = pymongo.MongoClient("mongodb+srv://banglajac13_db_user:ZGTKOUJTJloOFFQS@cluster0.wdbssln.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+
+ print("Success")
