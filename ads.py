622836780073f31ba833263868b950037020905c
mosharofroydev <mosharofroydev@gmail.com>
2025-09-05 02:24:10 +0000

    ads.py

--- ads.py
+++ ads.py
@@ -1,7 +1,11 @@
-# ads.py
+ # ads.py
+import random
 
 ad_links = [
     "🔥 আমাদের চ্যানেলে জয়েন করুন: https://t.me/your_channel",
     "💥 Sponsor Link: https://example.com/ad1",
     "👉 এখানে ক্লিক করুন: https://example.com/ad2"
 ]
+
+def random_ad():
+    return random.choice(ad_links)
