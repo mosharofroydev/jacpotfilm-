50c2fa1c84c8ef2c3098afb63fd1390282e0d788
mosharofroydev <mosharofroydev@gmail.com>
2025-09-12 14:22:20 +0600

    Update and rename database.py to config.py

---
+++ config.py
@@ -0,0 +1,17 @@
+class Config:
+    # Bot
+    BOT_TOKEN = "8210471056:AAEc76RNEX1w32M7WfyY3R8uKzEBy4aOb8spp"
+    ADMINS = [7170990925]
+
+    # Database
+    DATABASE_URL = "mongodb+srv://test:hvM1kLeXCCSXXb1u@cluster0.mrfgvxy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+    # Messages
+    START_MSG = """👋 হ্যালো {name}!
+
+আমি Auto Filter Bot 🎬
+
+Inline button দিয়ে সিনেমার ভিডিও সরাসরি পাবেন।  
+আপনার গ্রুপে সিনেমা search করতে নাম লিখুন।  
+"""
+    DELETE_AFTER_DAYS = 5
